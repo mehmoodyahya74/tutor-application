b4a425a53d34d548576ae5e21d0236c071dc1ead
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-02-20 04:46:59 -0800

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { storage } from "./storage.js";
-import { api } from "@shared/routes.js";
+import { api } from "shared/routes.js";
 import { z } from "zod";
 
 export function registerRoutes(app: Express) {
