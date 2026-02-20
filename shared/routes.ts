6c8fb423429d40f960554db991d6b7c3ba0a7017
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-02-20 05:08:50 -0800

    Update routes.ts

--- shared/routes.ts
+++ shared/routes.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { insertTutorApplicationSchema, tutorApplications } from './schema';
+import { insertTutorApplicationSchema, tutorApplications } from './schema.js';
 
 export const errorSchemas = {
   validation: z.object({
